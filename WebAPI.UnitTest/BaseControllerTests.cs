2088e5f1ad2efaf8ee9802d3370faaa9ad658ef3
Mert Kurt <tyrael91@hotmail.com>
2024-03-04 17:24:01 +0300

    UnitTest Commit 2

---
+++ WebAPI.UnitTest/BaseControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using Core.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Models.Dtos.ResponseDto;
+using WebAPI.Controllers;
+namespace WebAPI.UnitTest;
+
+public class BaseControllerTests
+{
+    private BaseController _controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _controller = new BaseController();
+    }
+
+    [Test]
+    public void ActionResultInstance_WhenStatusCodeOk_ReturnsOk()
+    {
+        //Arrange
+        var response = new Response<string>()
+        {
+            Data = "Test",
+            Message = "Test",
+            StatusCode = System.Net.HttpStatusCode.OK
+        };
+
+        //Act
+        var result = _controller.ActionResultInstance(response);
+
+        //Asset
+        Assert.IsInstanceOf<OkObjectResult>(result); //result'ın veri tipinin OkObjectResult tipinde olup olmadığına bakar. ActionResultInstance metodu return Ok() metodu döndürdüğü için ve Ok() metodu OkObjectResult nesnesi oluşturduğu için bu şekilde karşılaştırma yapılır.
+    }
+
+
+    [Test]
+    public void ActionResultInstance_WhenStatusCodeNotFound_ReturnsNotFound()
+    {
+        //Arrange
+        var response = new Response<string>()
+        {
+            Data = "Test",
+            Message = "Test",
+            StatusCode = System.Net.HttpStatusCode.NotFound
+        };
+        //Act
+        var result = _controller.ActionResultInstance(response);
+
+        //Assert
+        Assert.IsInstanceOf<NotFoundResult>(result);
+    }
+
+    [Test]
+    public void ActionResultInstance_WhenStatusCodeBadRequest_ReturnsBadRequest()
+    {
+        //Arrange
+        var response = new Response<string>()
+        {
+            Data = "Test",
+            Message = "Test",
+            StatusCode = System.Net.HttpStatusCode.BadRequest
+        };
+
+        //Act
+        var result = _controller.ActionResultInstance(response);
+
+        //Assert
+        Assert.IsInstanceOf<BadRequestObjectResult>(result);
+    }
+
+    [Test]
+    public void ActionResultInstance_WhenStatusCodeCreated_ReturnsCreated()
+    {
+        //Arrange
+        var response = new Response<string>()
+        {
+            Data = "Test",
+            Message = "Test",
+            StatusCode = System.Net.HttpStatusCode.Created
+        };
+
+        //Act
+        var result = _controller.ActionResultInstance(response);
+
+        //Assert
+        Assert.IsInstanceOf<CreatedResult>(result);
+    }
+
+
+}
--- projects/unitTest/Service.UnitTest/Products/ProductServiceTests.cs
+++ projects/unitTest/Service.UnitTest/Products/ProductServiceTests.cs
@@ -8,6 +8,9 @@ using Models.Dtos.ResponseDto;
 using System.Net;
 using Service.BusinessRules.Abstract;
 using Core.CrossCuttingConcerns.Exceptions;
+using Core.Shared;
+using NuGet.Frameworks;
+using System.Xml.XPath;
 
 namespace Service.UnitTest.Products
 {
@@ -19,7 +22,7 @@ namespace Service.UnitTest.Products
         private Mock<IProductRepository> _mockRepository;
         private Mock<IProductRules> _mockRules;
 
-
+        //bunlar sonradan eklendi, önce yukarıdakiler eklendi.
         private ProductAddRequest productAddRequest;
         private ProductUpdateRequest productUpdateRequest;
         private Product product;
@@ -83,5 +86,100 @@ namespace Service.UnitTest.Products
             Assert.AreEqual(result.StatusCode, System.Net.HttpStatusCode.BadRequest);
         }
 
+        [Test]
+        public void Delete_WhenProductIsPresent_ReturnsOk()
+        {
+            Guid id = new Guid();
+            //Arrange
+            _mockRules.Setup(x => x.ProductIsPresent(id));
+            _mockRepository.Setup(x => x.GetById(id, null)).Returns(product);
+            _mockRepository.Setup(x => x.Delete(product));
+
+            //Act
+            var result = _service.Delete(id);
+
+            //Assert
+            Assert.AreEqual(result.Data, productResponseDto);
+            Assert.AreEqual(result.StatusCode, System.Net.HttpStatusCode.OK);
+            Assert.AreEqual(result.Message, "Ürün Silindi");
+        }
+
+        [Test]
+        public void Delete_WhenProductIsNotPresent_ReturnsBadRequest()
+        {
+            //Arrange
+            Guid id = new Guid();
+            _mockRules.Setup(x => x.ProductIsPresent(id)).Throws(new BusinessException($"Id si : {id} olan ürün bulunamadı."));
+
+            //Act
+            var result = _service.Delete(id);
+
+            //Assert
+            Assert.AreEqual(result.StatusCode, System.Net.HttpStatusCode.BadRequest);
+            Assert.AreEqual(result.Message, $"Id si : {id} olan ürün bulunamadı.");
+        }
+
+        [Test]
+        public void GetAll_ReturnOk()
+        {
+
+            var products = new List<Product>()
+            {
+                product
+            };
+
+            var responses = new List<ProductResponseDto>()
+            {
+                productResponseDto
+            };
+
+            //Arrange
+            _mockRepository.Setup(x => x.GetAll(null, null)).Returns(products); //IProductRepository'nin yani 
+
+            //Action
+            var result = _service.GetAll(); //ProductService'in GetAll() metodu çalıştırıldığında ilk satırda ProductRepository'nin GetAll() metodu çalıştırılacak ve List<Product> dönecek. Bu dönecek olan List<Product>'ı yukarıda Arrange ile belirttik. Ondan sonra servisteki GetAll metodu devam ediyor ve yukarıdaki products değişkeninde bulunan product'lara Select işlemi uygulayıp List<ProductResponseDto> dönüyor. Ondan sonra devam edip; return new Response<List<ProductResponseDto>>() dönüyor. Bu dönen return new Response<List<ProductResponseDto>>() değerleri ile Aşağıda Assert ile belirttiğimiz değerler uyuşuyorsa testten geçiyor (ki aynı değerleri biz veriyoruz, yukarıda oluşturduğumuz değişkenler ile).
+
+            //Assert
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+            Assert.AreEqual(result.Data, responses);
+        }
+
+        [Test]
+        public void GetByDetailId_WhenDetailIsPresent_ReturnsOk()
+        {
+            ProductDetailDto dto = new ProductDetailDto()
+            {
+                CategoryName = "Test",
+                Id = new Guid(),
+                Name = "Test",
+                Price = 1000,
+                Stock = 25
+            };
+
+            //Arrange
+            _mockRules.Setup(x => x.ProductIsPresent(It.IsAny<Guid>())); //herhangi bir Guid vermek istemiyorsak bu şekilde de yazabiliriz. ProductIsPresent metodu void olduğu için aşağıdaki gibi .Returns yazmaya gerek yok. Bu satırı yorum satırına alsak dahi testten geçer.
+            _mockRepository.Setup(x => x.GetProductDetail(It.IsAny<Guid>())).Returns(dto); //bu metodu çalıştırdıktan sonra bana yukarıdaki dto nesnesini dön.
+
+            //Action
+            var result = _service.GetByDetailId(It.IsAny<Guid>());
+
+            //Assert
+            Assert.AreEqual(result.Data, dto);
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void GetByDetailId_WhenDetailIsNotPresent_ReturnsBadRequest()
+        {
+            //Arrange
+            _mockRules.Setup(x => x.ProductIsPresent(It.IsAny<Guid>())).Throws(new BusinessException($"Id si : {It.IsAny<Guid>()} olan ürün bulunamadı."));
+
+            //Act
+            var result = _service.GetByDetailId(It.IsAny<Guid>());
+
+            //Assert
+            Assert.AreEqual(result.Message, $"Id si : {It.IsAny<Guid>()} olan ürün bulunamadı.");
+            Assert.AreEqual(result.StatusCode, HttpStatusCode.BadRequest);
+        }
     }
 }
